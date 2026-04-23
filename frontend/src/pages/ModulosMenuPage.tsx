69528d934d3a3c814400647bdc064d139327a190
victorhugogestaup <victorhugo.gestaup@gmail.com>
2026-04-23 10:18:03 -0400

    Remover import nao utilizado Button do ModulosMenuPage

--- frontend/src/pages/ModulosMenuPage.tsx
+++ frontend/src/pages/ModulosMenuPage.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom'
-import { Button } from '../components/ui'
 import { CADERNETAS, LOGO_URL } from '../utils/constants'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
